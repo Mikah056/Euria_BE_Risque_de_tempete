72a73d2d8a6a274dc53969915e17bd3e47d3984b
Hakim Mounde <116036655+Mikah056@users.noreply.github.com>
2022-11-10 22:39:44 +0000

    Update code_traitement_BD.R

--- code_traitement_BD.R
+++ code_traitement_BD.R
@@ -1,13 +1,13 @@
 ###########  Traitement de la base  ###########
 
 
-<<<<<<< HEAD
+
 ######################importation des 73 bases data-events-Details
-=======
+
 
 
 #importation des 73 bases data-events-Details
->>>>>>> 097f81306eec8222e372e4ecc40a39d34425dc19
+
 Alldata=list(73)
 for(i in 1950:1971){
   Alldata[[i]]=read.csv(paste0("C:/Hakim/D1/Euria/M1/BE/Data/Storm Events _ Details/StormEvents_details-ftp_v1.0_d",i,"_c20210803.csv"),header=T)
@@ -34,7 +34,7 @@ for (i in 1950:2022) {
   Alldata1[[i]]=Alldata[[i]][,-c(3,6,7,10,14,15,16,17,18,19,20,27,28,29,30,31,35,36,37,38,49,50,51)]
 }
 
-#Suppression des lignes des ï¿½venements autres que les tornades
+#Suppression des lignes des Ã¯Â¿Â½venements autres que les tornades
 Alldata2=NULL
 for (i in 1950:2022) {
   Alldata2[[i]]=Alldata1[[i]][-which(Alldata1[[i]]$EVENT_TYPE!='Tornado'),]
@@ -64,7 +64,7 @@ p = ggplot(data=data_na[c(16,17,18,19,20,21,22,23,24,25,26,27)], aes(y=data_na))
   geom_bar(stat='identity')
 p
 
-#Pourcentage des observations manquantes après 1985
+#Pourcentage des observations manquantes aprÃ¨s 1985
 datam=Data[,c(16,17,18,19,20,21,22,23,24,25,26,27)]
 (apply(apply(datam[which(Data$YEAR>1985),],2,is.na),2,sum)/apply(apply(datam,2,is.na),2,sum))*100
 apply(apply(datam[which(Data$YEAR>1995),],2,is.na),2,sum)
@@ -100,7 +100,7 @@ for (i in 1950:2022) {
   Alldata1[[i]]=Alldata[[i]][,-c(3,6,7,10,14,15,16,17,18,19,20,27,28,29,30,31,35,36,37,38,49,50,51)]
 }
 
-#Suppression des lignes des ï¿½venements autres que les tornades
+#Suppression des lignes des Ã¯Â¿Â½venements autres que les tornades
 Alldata2=NULL
 for (i in 1950:2022) {
   Alldata2[[i]]=Alldata1[[i]][-which(Alldata1[[i]]$EVENT_TYPE!='Tornado'),]
@@ -160,7 +160,7 @@ Alldata0[[2020]]=read.csv("C:/Hakim/D1/Euria/M1/BE/Data/Storm Events _ Locations
 Alldata0[[2021]]=read.csv("C:/Hakim/D1/Euria/M1/BE/Data/Storm Events _ Locations/StormEvents_Locations-ftp_v1.0_d2021_c20220921.csv",header=T)
 Alldata0[[2022]]=read.csv("C:/Hakim/D1/Euria/M1/BE/Data/Storm Events _ Locations/StormEvents_Locations-ftp_v1.0_d2022_c20220921.csv",header=T)
 
-#Ces bases sont vides en deçà de 1996
+#Ces bases sont vides en deÃ§Ã  de 1996
 View(Alldata0[[1996]])
 names(Alldata0[[2021]])
 p=NULL
@@ -174,7 +174,7 @@ for (i in 1997:2022) {
   DataL=rbind(DataL,Alldata0[[i]])
 }
 
-#Suppression des lignes des évenements autres que les tornades à partir de EVENT_ID
+#Suppression des lignes des Ã©venements autres que les tornades Ã  partir de EVENT_ID
 z=NULL
 for (i in 1:nrow(DataL)) {
   z=rbind(z,DataL[which(DataL$EVENT_ID[i]==Data$EVENT_ID),])
@@ -217,21 +217,21 @@ write.csv(z,file='C:/Hakim/D1/Euria/M1/BE/Data/Base_BE2.csv',row.names = FALSE)
 #####################  Traitement des valeurs manquantes
 
 
-#Les bases Locations sont vides en deçà de 1972; on peut supposer que les coordonnées
-#n'étaient pas encore enregistrés de façon précise, et ont été estimés à partir des
-#différentes sources d'information et des récits sur les évènements.
+#Les bases Locations sont vides en deÃ§Ã  de 1972; on peut supposer que les coordonnÃ©es
+#n'Ã©taient pas encore enregistrÃ©s de faÃ§on prÃ©cise, et ont Ã©tÃ© estimÃ©s Ã  partir des
+#diffÃ©rentes sources d'information et des rÃ©cits sur les Ã©vÃ¨nements.
 
 #On a vu qu'au dela de 1995, locations a plus de NA que Details
 
-##Répartition des NA par année
+##RÃ©partition des NA par annÃ©e
 Nombre_NA=c()
 for (i in 1955:2022) { 
   Nombre_NA=c(Nombre_NA,sum(apply(apply(datam[which(Data$YEAR==i),],2,is.na),2,sum)))
 }
 annee=1955:2022
 v1=cbind(annee,Nombre_NA)
-plot(v1,main="Répartition des NA par année")
-lines(v1,col="blue",main="Répartition des NA par année")
+plot(v1,main="RÃ©partition des NA par annÃ©e")
+lines(v1,col="blue",main="RÃ©partition des NA par annÃ©e")
 
 #Proportion de NA dans chaque variable
 l=nrow(datam[which(Data$YEAR>=2000),])
@@ -244,43 +244,43 @@ write.csv(na2,file='C:/Hakim/D1/Euria/M1/BE/Data/na2.csv',row.names = TRUE)
 
 
 
-#Nombre de tornade n'ayant pas d'information d'arrivée sachant qu'elles n'ont pas
-#d'information de départ
+#Nombre de tornade n'ayant pas d'information d'arrivÃ©e sachant qu'elles n'ont pas
+#d'information de dÃ©part
 length(Data$END_LON[which(is.na(Data$BEGIN_LON))])
 
 
-##Evolution des fréquences de tornade
+##Evolution des frÃ©quences de tornade
 frequence=c()
 for (i in 1980:2022) {
   frequence=c(frequence,sum(Data$YEAR==i))
 }
 v2=cbind(annee,frequence)
 plot(v2)
-lines(v2,col="red",main="Evolution des fréquences de tornade")
+lines(v2,col="red",main="Evolution des frÃ©quences de tornade")
 
 
-#Proportion des tornades dont nous n'avons aucune information géographique
+#Proportion des tornades dont nous n'avons aucune information gÃ©ographique
 (1191/length(Data$YEAR>=1980))*100
-##On se propose de les supprimer et d'appliquer les méthodes de lissage aux autres valeurs manquantes
+##On se propose de les supprimer et d'appliquer les mÃ©thodes de lissage aux autres valeurs manquantes
 
 
 
 
 
-#Suppression des individus n'ayant pas d'observation au départ 
+#Suppression des individus n'ayant pas d'observation au dÃ©part 
 length(which(is.na(Data$BEGIN_LAT) & is.na(Data$BEGIN_LON) & is.na(Data$END_LAT) & is.na(Data$END_LON)))
 Data = Data[-which(is.na(Data$BEGIN_LON)),]
 
 
 ##Staistiques descriptives
 
-#Evolution des fréquence au fil des années
+#Evolution des frÃ©quence au fil des annÃ©es
 p1=table(Data$YEAR)
 p2=ts(p1, start=1950, end=2022, frequency=1)
 plot(p2)
 summary(summary(as.factor(Data$YEAR)))
 
-#Evolution de la sévérité au fil des années
+#Evolution de la sÃ©vÃ©ritÃ© au fil des annÃ©es
 
 
 
